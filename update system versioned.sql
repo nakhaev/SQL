65174a82ae9954d90d2545722e13ea1f179d703b
Oleksii Nakhaiev <oleksii.nakhaiev@digitalchampion.net>
2023-04-07 07:48:31 +0300

    copy from prod

--- update system versioned.sql
+++ update system versioned.sql
@@ -1,13 +1,13 @@
-ALTER TABLE [ods].[DocumentInfo] SET (SYSTEM_VERSIONING = OFF);
-ALTER TABLE [ods].[DocumentInfo] DROP PERIOD FOR SYSTEM_TIME;
-ALTER TABLE [ods].[DocumentInfo] DROP CONSTRAINT [DF_DocumentInfo_ValidFrom];
-ALTER TABLE [ods].[DocumentInfo] DROP CONSTRAINT [DF_DocumentInfo_ValidTo];
-ALTER TABLE [ods].[DocumentInfo] DROP COLUMN ValidFrom;
-ALTER TABLE [ods].[DocumentInfo] DROP COLUMN ValidTo;
-ALTER TABLE [ods].[DocumentInfo] ADD [ValidFrom] [datetime2](2) NOT NULL;
-ALTER TABLE [ods].[DocumentInfo] ADD [ValidTo] [datetime2](2) NOT NULL;
-ALTER TABLE [ods].[DocumentInfo] ADD PERIOD FOR SYSTEM_TIME (ValidFrom, ValidTo);
-DROP TABLE  [ods].[DocumentInfo_History];
-ALTER TABLE [ods].[DocumentInfo] SET (SYSTEM_VERSIONING = ON (HISTORY_TABLE=[ods].[DocumentInfo_History]));
-ALTER TABLE [ods].[DocumentInfo] ADD  CONSTRAINT [DF_DocumentInfo_ValidFrom]  DEFAULT (dateadd(second,(-1),sysutcdatetime())) FOR [ValidFrom]
-ALTER TABLE [ods].[DocumentInfo] ADD  CONSTRAINT [DF_DocumentInfo_ValidTo]  DEFAULT ('9999.12.31 23:59:59.99') FOR [ValidTo]
+ALTER TABLE [mdm].[Deals] SET (SYSTEM_VERSIONING = OFF);
+ALTER TABLE [mdm].[Deals] DROP PERIOD FOR SYSTEM_TIME;
+ALTER TABLE [mdm].[Deals] DROP CONSTRAINT [DF_Deals_ValidFrom];
+ALTER TABLE [mdm].[Deals] DROP CONSTRAINT [DF_Deals_ValidTo];
+ALTER TABLE [mdm].[Deals] DROP COLUMN ValidFrom;
+ALTER TABLE [mdm].[Deals] DROP COLUMN ValidTo;
+ALTER TABLE [mdm].[Deals] ADD [ValidFrom] [datetime2](2) NOT NULL;
+ALTER TABLE [mdm].[Deals] ADD [ValidTo] [datetime2](2) NOT NULL;
+ALTER TABLE [mdm].[Deals] ADD PERIOD FOR SYSTEM_TIME (ValidFrom, ValidTo);
+DROP TABLE  [mdm].[Deals_History];
+ALTER TABLE [mdm].[Deals] SET (SYSTEM_VERSIONING = ON (HISTORY_TABLE=[mdm].[Deals_History]));
+ALTER TABLE [mdm].[Deals] ADD  CONSTRAINT [DF_Deals_ValidFrom]  DEFAULT (dateadd(second,(-1),sysutcdatetime())) FOR [ValidFrom]
+ALTER TABLE [mdm].[Deals] ADD  CONSTRAINT [DF_Deals_ValidTo]  DEFAULT ('9999.12.31 23:59:59.99') FOR [ValidTo]
